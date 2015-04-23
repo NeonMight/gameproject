36f886eff2b5db4d1a11a453b78218abe40b6c17
NeonMight <srh10a@acu.edu>
2015-04-23 18:40:27 -0500

    User cards are now kept track of in associative array

--- blackjackserver.js
+++ blackjackserver.js
@@ -38,6 +38,7 @@ var rooms = ['Lobby'];
 var decks = [];
 var usernames = {};
 var dealerCards = [];
+var userCards = {};
 var winningUser = [];  // current winning user for each room
 var winningHand = []; //current high hand for each room
 var inRoom = [];
@@ -63,7 +64,6 @@ app.use(express.static(__dirname+'/'));
 //events will be adduser, disconnect, turn, hitme, pass
 io.sockets.on('connection', function(socket)
 {
-  var hand = [];
   var seat = 0;
   socket.on('adduser',function(username) //when a new user connects, welcome that user and send them the how to play message
   {
@@ -87,7 +87,6 @@ io.sockets.on('connection', function(socket)
       inRoom[socket.room].push("dealer"); //now push dealer
       roomCount++;
     }
-    console.log("Users in room"+socket.room+" are "+inRoom[socket.room]);
     //dealer cards
     socket.emit('ready',username);
     seat = roomPopulation[socket.room];
@@ -104,23 +103,29 @@ io.sockets.on('connection', function(socket)
   socket.on('init',function(usr) //sets up initial game
   {
     var offset = 0
-    var position = 60 + (175 * seat);
+    var position = 100; // THIS is what you need to edit to figure out proper offset leves
     io.sockets.in('room'+socket.room).emit('dealer',{x:350+offset, y:125, val:dealerCards[socket.room][0]});
-    io.sockets.in('room'+socket.room).emit('dealer',{x:350+75, y:125, val:dealerCards[socket.room][1]});
-    for (var i = 0; i < 2;i++)
+    io.sockets.in('room'+socket.room).emit('dealer',{x:350+50, y:125, val:dealerCards[socket.room][1]});
+    userCards[usr] = [];
+    userCards[usr].push(decks[socket.room].pop());
+    userCards[usr].push(decks[socket.room].pop());  //deal first 2 cards for user
+    for (var i = 0; i < inRoom[socket.room].length-1; i++) //for each user
     {
       // store guest cards in another array and iterate to get existing user's cards
-      var guestcard = decks[socket.room].pop();
-      hand.push(guestcard);
-      io.sockets.in('room'+socket.room).emit('card',{x:position+offset, y:400, val:guestcard, user:usr});
-      offset += 75;
+      //var guestcard = decks[socket.room].pop();
+      //io.sockets.in('room'+socket.room).emit('card',{x:position+offset, y:400, val:guestcard, user:usr});
+      //offset += 75;
+      // iterate over saved cards and output
+      var name = inRoom[socket.room][i] // use i to index into players array
+      for (var j = 0; j < userCards[name].length; j++) //for each card
+      {
+        io.sockets.in('room'+socket.room).emit('card', {x:position+offset/*won't work yet; needs tweaking*/, y:400, val:userCards[name][j], user:name}); // send each card for this user
+        offset += 50; //increment CARD offset for this player
+      }
+      // NOW, increment the PLAYER offset here
+      position += 175; //will this work???
+      offset = 0;
     }
-    var total = 0;
-    for (var i = 0; i < hand.length; i++)
-    {
-      total += hand[i];
-    }
-    console.log(usr+' has a hand of '+total);
     if (roomPopulation[socket.room] >= 2)
     {
       io.sockets.in('room'+socket.room).emit('turn', inRoom[socket.room][0]); //the first user in room
